34f2391ddef2adf9057c4a54de5f0872893fbca0
Garming <garming_sam@outlook.com>
2014-09-07 21:50:49 +1200

    Add what there is to know about json submission event

---
+++ River Watch/SubmitEvent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace River_Watch
+{
+    class SubmitEvent
+    {
+        public SubmitEvent()
+        {
+
+
+        }
+
+        // I would heavily suggest a JSON library at some point
+        public String createJSONSubmit()
+        {
+            StringBuilder s = new StringBuilder("{");
+            s.Append("\"geolocation\":");
+            s.Append("{");
+            s.Append("\"lat\":"); s.Append("3");
+            s.Append(",");
+            s.Append("\"long\":"); s.Append("4");
+            s.Append("}");
+
+            s.Append(",");
+
+            s.Append("\"description\":"); s.Append("\"some description\"");
+
+            s.Append(",");
+
+            // From the Android code, it seems to be doubly-arrayed
+            s.Append("\"tags\":[[");
+            s.Append("\"tag1\"");
+                s.Append(",");
+                s.Append("\"tag2\"");
+            s.Append("]]");
+
+            s.Append(",");
+            s.Append("\"physical_location\":"); s.Append("\"addresss\"");
+
+            s.Append("}");
+            return s.ToString();
+        }
+
+    }
+}
