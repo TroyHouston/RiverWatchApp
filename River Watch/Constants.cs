8d6e6d023247d89ccf620004da27d6f7264da65c
Garming <garming_sam@outlook.com>
2014-09-07 21:09:32 +1200

    Add a constants file

---
+++ River Watch/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace River_Watch
+{
+    static class Constants
+    {
+        public static string SERVER_URL { get { return "http://www.wainz.org.nz"; } }
+        public static string SECURE_SERVER_URL { get { return "https://www.wainz.org.nz"; } }
+
+        public static string SUBMIT_PATH { get { return SERVER_URL + "/api/image" ; } }
+        public static string SECURE_SUBMIT_PATH { get { return SERVER_URL + "/api/image"; } }
+    }
+}
