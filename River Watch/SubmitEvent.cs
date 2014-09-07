823f68a30c7fcafe2eff32203936f53df385ceb1
Garming <garming_sam@outlook.com>
2014-09-08 03:29:08 +1200

    Add some commented out HTTP web request code

--- River Watch/SubmitEvent.cs
+++ River Watch/SubmitEvent.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace River_Watch
 {
@@ -43,6 +45,48 @@ namespace River_Watch
 
             s.Append("}");
             return s.ToString();
+        }
+
+        public void send(Stream fileStream) {
+            // Reset the stream position to the beginning
+            fileStream.Position = 0;
+
+            /* Some example code
+             * I think we need to use http client or httpwebrequest
+             * 
+            using (HttpClient client = new HttpClient())
+            {
+
+                using (MultipartFormDataContent content = new MultipartFormDataContent())
+                {
+                    if (request.Headers != null)
+                    {
+                        foreach (KeyValuePair<string, string> header in request.Headers)
+                        {
+                            content.Headers.Add(header.Key, header.Value);
+                        }
+                    }
+                    using (StreamContent streamContent = new StreamContent(photoStream))
+                    {
+                        streamContent.Headers.ContentType = MediaTypeHeaderValue.Parse(request.ContentType);
+                        content.Add(streamContent, multipartName, fileName);
+
+                        // Post the message and ensure a result.
+                        using (HttpResponseMessage response = await client.PostAsync(request.TargerUrl, content))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string responseData = await response.Content.ReadAsStringAsync();
+                                return new RestResponse(ResponseStatus.Success, null) { ResponseData = responseData };
+                            }
+                            else
+                            {
+                                return new RestResponse(ResponseStatus.Fail, null) { Exception = new Exception(response.ReasonPhrase) };
+                            }
+                        }
+                    }
+                }
+            }*/
         }
 
     }
