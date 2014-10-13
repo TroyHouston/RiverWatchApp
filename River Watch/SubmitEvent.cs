5a010f55acd3e1fdc9a0cefa7a1d915de4d8c549
falla1 <marcshaw888@hotmail.com>
2014-10-14 00:11:41 +1300

    Fixed where to send the file

--- River Watch/SubmitEvent.cs
+++ River Watch/SubmitEvent.cs
@@ -178,7 +178,7 @@ namespace River_Watch
             Uri downloadUri = new Uri("shared/transfers/" + filename, UriKind.RelativeOrAbsolute);
             
             /* !!!!! FOR USE IN PRODUCTION !!!!!! */
-            BackgroundTransferRequest transferRequest = new BackgroundTransferRequest(new Uri(Constants.SERVER_URL + Constants.SUBMIT_PATH));
+            BackgroundTransferRequest transferRequest = new BackgroundTransferRequest(new Uri(Constants.SUBMIT_PATH));
            
             /* !!!!! FOR USE IN TESTING !!!!! */
             //BackgroundTransferRequest transferRequest = new BackgroundTransferRequest(new Uri("http://192.168.1.91:8000/api/image"));
